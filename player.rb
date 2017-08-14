8a47b20b026624ef55b5405a7ca0304f5f0db92a
KaiserChan <chankaiserkk@gmail.com>
2017-08-14 15:26:12 -0400

    Updated player.rb - Created class

--- player.rb
+++ player.rb
@@ -1,5 +1,12 @@
 class Player
 
-
+def initialize(lives, gold_coins, health_points)
+  @lives = lives
+  @gold_coins = gold_coins
+  @health_points = health_points
+end
 
 end
+
+
+kaiser = Player.new(5, 0, 10)
