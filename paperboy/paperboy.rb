926105b6a125eb584f8bd61861eda42e063a1670
KaiserChan <chankaiserkk@gmail.com>
2017-08-14 17:34:28 -0400

    Created paperboy.rb - Created class

--- paperboy/paperboy.rb
+++ paperboy/paperboy.rb
@@ -1,3 +1,18 @@
 class Paperboy
+  def initialize(name, experience, earnings)
+    @name = name
+    @experience = experience
+    @earnings = earnings
 
+    @minimum = 50
+    @quota = @minimum + (@experience / 2)
+  end
+
+  def quota
+
+
+  end
 end
+
+
+johnny = Paperboy.new("Johnny", 0, 0)
